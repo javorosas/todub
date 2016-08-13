d40fc01b7a84d2d3cdf1b01f8ea24a2f2354f091
Javier Rosas <javier.rosasb@gmail.com>
2016-08-12 21:22:22 -0700

    add styles

---
+++ src/taskInput/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: 'white',
+    padding: 10,
+    elevation: 2,
+    margin: 10
+  }
+});
+
+export default styles;
---
+++ src/taskList/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  list: {
+    // width: 100
+    // backgroundColor: 'black'
+    // height: 300
+  }
+});
+
+export default styles;
