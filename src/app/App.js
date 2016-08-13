61dd1390519d1273e55a910543d7e858ed8ca861
Javier Rosas <javier.rosasb@gmail.com>
2016-08-13 03:30:46 -0700

    make compatible with ios

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,7 @@
+/*eslint jsx-quotes: ["error", "prefer-double"]*/
+
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
 import TaskInputContainer from '../taskInput/TaskInputContainer';
 import TaskListContainer from '../taskList/TaskListContainer';
 
@@ -7,6 +9,10 @@ export default class App extends Component {
   render () {
     return (
       <View style={styles.container}>
+        <StatusBar
+          backgroundColor="blue"
+          barStyle="light-content"
+        />
         <TaskListContainer />
         <TaskInputContainer />
       </View>
