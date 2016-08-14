515853ef761a533078eaae8fbf1415ddef49218a
Javier Rosas <javier.rosasb@gmail.com>
2016-08-14 14:04:36 -0700

    hotfix: add thunk middleware to ios too

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,17 @@
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './src/app/App';
 import tasks from './src/app/reducer';
 
-let store = createStore(tasks);
+const loggerMiddleware = createLogger();
+let store = createStore(tasks, applyMiddleware(
+  thunkMiddleware,
+  loggerMiddleware
+));
 
 class todub extends Component {
   render () {
